1a1243b421f3c3e64adc007f22062296f0d1cf49
Anthony Mahramus <anthony.mahramus@gmail.com>
2019-09-30 13:42:32 -0400

    Add 'start' function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,4 +27,24 @@ var game = {
             game.done();
         }
     },
+
+    start: function() {
+        timer = setInterval(game.countdown, 1000);
+
+        $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+        $("#start").remove();
+
+        for (var i = 0; i < questions.length; i++) {
+            panel.append("<h2>" + questions[i].question + "</h2>");
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                panel.append("<input type='radio' name='question-" + i +
+                "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+        }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+
+},
+
 };
